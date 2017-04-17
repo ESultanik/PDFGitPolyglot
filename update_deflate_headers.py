9b99bc15cd1bc9ac125f076a493ff31e5f191ab3
Evan Sultanik <evan@sultanik.com>
2017-04-17 15:23:57 -0400

    Got the SHA1 repair working!

--- update_deflate_headers.py
+++ update_deflate_headers.py
@@ -34,7 +34,7 @@ def parse_pack_object(data):
     decompressed = d.decompress(data[header_bytes:], length)
     assert len(decompressed) == length
     compressed_length = len(data) - header_bytes - len(d.unused_data)
-    return header_bytes, obj_type, length, compressed_length
+    return header_bytes, obj_type, length, compressed_length, decompressed
 
 def fix_pack_sha1(pdf_content, pdf_header_offset):
     pack_offset = pdf_content.rindex("PACK", 0, pdf_header_offset)
@@ -43,36 +43,18 @@ def fix_pack_sha1(pdf_content, pdf_header_offset):
     offset = pack_offset + 12
     for i in range(num_objects):
         #print "Offset: 0x%x" % offset
-        header_bytes, obj_type, decompressed_length, compressed_length = parse_pack_object(pdf_content[offset:])
+        header_bytes, obj_type, decompressed_length, compressed_length, decompressed = parse_pack_object(pdf_content[offset:])
         #print "Parsed pack object at offset 0x%x of type %d with a %d byte header, %d byte body (decompressed), and %d byte body (compressed)" % (offset, obj_type, header_bytes, decompressed_length, compressed_length)
         offset += header_bytes + compressed_length
     print "SHA1 should be at offset 0x%x" % offset
-    sha1 = hashlib.sha1(pdf_content[:offset])
+    sha1 = hashlib.sha1(pdf_content[pack_offset:offset])
     print sha1.hexdigest()
     if sha1.digest() == pdf_content[offset:offset+20]:
         print "SHA1 is valid!"
     else:
-        print "SHA1 is not valid!"
-    return offset
-    # sha1 = None
-    # last_percent = -1
-    # for i in range(pack_offset + 12 + 20, len(pdf_content) - 20):
-    #     percent = int(float(i - (pack_offset + 12 + 20)) / float(len(pdf_content)-20-(pack_offset + 12 + 20)) * 100.0)
-    #     if percent > last_percent:
-    #         sys.stdout.write('\r' + ' ' * 30 + "\rSearching for end of pack... %d%%" % percent)
-    #         sys.stdout.flush()
-    #         last_percent = percent
-    #     pack_content = pdf_content[pack_offset:i]
-    #     test = hashlib.sha1(pack_content)
-    #     assert len(test.digest()) == 20
-    #     #print test.digest(), pdf_content[i:i+20]
-    #     if test.digest() == pdf_content[i:i+20]:
-    #         sha1 = i
-    #         break
-    # if sha1 is not None:
-    #     print "\nFound SHA1 at offset 0x%x!" % sha1
-    # else:
-    #     print "\nDid not find SHA1!"
+        print "SHA1 is not valid! Repairing it..."
+        pdf_content = pdf_content[:offset] + sha1.digest() + pdf_content[offset+20:]
+    return pdf_content
     
 def read_deflate_header(header):
     last = bool(0b1 & ord(header[0]))
@@ -99,7 +81,8 @@ def update_deflate_headers(pdf_content, output, block_offsets):
         raise Exception("Could not find PDF header!")
     pdf_header_offset = len(m.group(1))
     print "Found PDF header at offset %d" % pdf_header_offset
-    fix_pack_sha1(pdf_content, pdf_header_offset)
+    initial_repair = fix_pack_sha1(pdf_content, pdf_header_offset)
+    assert initial_repair == pdf_content # Make sure the input has a valid SHA1
     content_before = zlib.decompress(pdf_content[pdf_header_offset - 7:])
     deflate_header = pdf_content[:pdf_header_offset][-5:]
     last, length = read_deflate_header(deflate_header)
@@ -130,7 +113,7 @@ def update_deflate_headers(pdf_content, output, block_offsets):
     print "Validating the resulting DEFLATE headers..."
     if content_before != content_after:
         raise Exception("Error: the updated DEFLATE output is corrupt!")
-    fix_pack_sha1(pdf_content, pdf_header_offset)
+    pdf_content = fix_pack_sha1(pdf_content, pdf_header_offset)
     out.write(pdf_content)
     out.flush()
 
